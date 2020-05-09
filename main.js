4815734e962ddfa0cbb47dfdb03584d682bd6f32
saseb80 <saseb80@gmail.com>
2020-05-09 14:34:36 -0500

    segundo commit de prueba

--- main.js
+++ main.js
@@ -12,19 +12,27 @@ bot.once('ready', ()=>{
 //on siempre escucha
 bot.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(' ');
+     
+    const args = message.content.slice(prefix.length).split(/ +/);   //El + es para evitar que cuente espacios extras en el comando
     const command = args.shift().toLowerCase();
     
     //se usan `` para cuando se quiera agregar una variable seguido de ${variable} 
     if(message.content === `${prefix}info`){
         message.channel.send(`Nombre Servidor: ${message.guild.name}\nDueño: ${message.guild.owner} :]`);  //guild se refiere al server
-    }
+    }  
     else if(message.content === `${prefix}server`){
         message.channel.send(`${message.guild.name}`);
     }
     else if(message.content === `${prefix}user`){
         message.channel.send(`User: ${message.author.username}\nID: ${message.author.id}`);
+    }
+    else if(command === 'kick'){
+        const taggedUser = message.mentions.users.first();
+        if (!message.mentions.users.size) { //esto es para evitar que 
+            return message.reply('you need to tag a user in order to kick them!');
+        }
+        message.channel.send(`kick ${taggedUser}`);
+
     }
 });
 
